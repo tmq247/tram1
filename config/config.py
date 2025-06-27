e3cdb31649843c1f3d21a358dfc57c9fbe43c06b
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-06-28 01:05:15 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -101,7 +101,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/tmq247/tram1",
+    "https://github.com",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "alpha")
 
