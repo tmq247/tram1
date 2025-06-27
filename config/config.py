69540fb314781a2f76f2194b09c38613da14a8f4
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-06-27 23:10:48 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -142,7 +142,7 @@ TELEGRAM_DOWNLOAD_EDIT_SLEEP = int(getenv("TELEGRAM_EDIT_SLEEP", "5"))
 
 
 # Your Github Repo.. Will be shown on /start Command
-GITHUB_REPO = getenv("GITHUB_REPO", "https://github.com/TheTeamVivek/YukkiMusic")
+GITHUB_REPO = getenv("GITHUB_REPO", "https://github.com/tmq247/tram1")
 
 
 # Spotify Client.. Get it from https://developer.spotify.com/dashboard
