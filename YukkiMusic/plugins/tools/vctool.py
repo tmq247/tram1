4796ee880e48f3e4623e8e3a77fb5344a6ac481c
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-06-30 02:10:04 +0700

    Update vctool.py

--- YukkiMusic/plugins/tools/vctool.py
+++ YukkiMusic/plugins/tools/vctool.py
@@ -212,6 +212,7 @@ async def start_group_call(c: Client, m: Message):
         if bot_member.status not in [ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.OWNER]:
             return await app.send_message(chat_id, f"""❌ Userbot không có quyền mở call trong nhóm này. Thêm userbot @{assum} làm qtv với quyền quản lý call.""")
         if not bot_member.privileges or not bot_member.privileges.can_manage_video_chats:
+            print(bot_member.privileges)
             return await app.send_message(chat_id, f"""⚠️ Userbot @{assum} là quản trị viên nhưng không có quyền quản lý call.""")
     except Exception as e:
         return await app.send_message(chat_id, f"Đã xảy ra lỗi khi kiểm tra quyền của userbot: {e}")
